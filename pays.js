939d492f914fe13073359687cd1e533a52311565
Jules Fournier <jules.fournier.1210@gmail.com>
2021-02-17 15:17:18 +0100

    init

---
+++ pays.js
@@ -0,0 +1,28 @@
+const pays = [
+  {
+    nom: 'The office',
+    flag: '1.webp',
+  },
+  {
+    nom: 'Greys Anatomy',
+    flag: '2.webp',
+  },
+  {
+    nom: 'Big Mouth',
+    flag: '3.webp',
+  },
+  {
+    nom: 'New Girl',
+    flag: '4.webp',
+  },
+  {
+    nom: 'Stranger Things',
+    flag: '5.webp',
+  },
+  {
+    nom: 'Modern Family',
+    flag: '6.webp',
+  },
+]
+
+module.exports = pays
---
+++ router.js
@@ -0,0 +1,51 @@
+var express = require('express')
+const { indexOf } = require('./pays')
+var router = express.Router()
+
+const pays = require('./pays')
+
+const paysAleatoire = () => {
+  return pays[Math.floor(Math.random() * pays.length)]
+}
+
+const quatreDrapeauxAleatoires = () => {
+  const quatreDrapeauxAleatoires = []
+  while (quatreDrapeauxAleatoires.length < 4) {
+    let paysGenere = paysAleatoire()
+    if (quatreDrapeauxAleatoires.indexOf(paysGenere) < 0) {
+      quatreDrapeauxAleatoires.push(paysGenere)
+    }
+  }
+  return quatreDrapeauxAleatoires
+}
+
+router.get('/', (req, res, next) => {
+  res.render('home')
+})
+
+router.get('/play', (req, res, next) => {
+  const quatrePays = quatreDrapeauxAleatoires()
+  const drapeauADeviner = quatrePays[Math.floor(Math.random() * 4)].flag
+  const quatreNomsDePays = quatrePays.map(pays => pays.nom)
+  res.render('index', { drapeauADeviner, quatreNomsDePays })
+})
+
+const corriger = (reponseUtilisateur) => {
+  const bonneReponse = pays.find(element => element.flag === reponseUtilisateur.drapeau)
+  return {
+    reponseEstValide: reponseUtilisateur.pays === bonneReponse.nom,
+    pays: bonneReponse.nom,
+    drapeau: bonneReponse.flag
+  }
+}
+
+router.post('/correction', (req, res, next) => {
+  const reponse = {
+    pays: req.body.pays,
+    drapeau: req.body.drapeau
+  }
+  const correction = corriger(reponse)
+  res.render('correction', { correction })
+})
+
+module.exports = router
